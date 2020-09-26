65ec549ec461e52aed35f4d89a4a787a102e2842
MatheusHuk <matheushukmoreschi2000@gmail.com>
2020-09-26 18:18:14 -0300

    Base

---
+++ app/src/main/java/br/com/bandtec/gespo/SplashActivity.kt
@@ -0,0 +1,22 @@
+package br.com.bandtec.gespo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.schedule
+
+class SplashActivity : AppCompatActivity() {
+    val valor = 1;
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash)
+        check();
+    }
+
+    private fun check(){
+        if(this.valor === 1){
+            setContentView(R.layout.activity_main)
+        }
+    }
+}
