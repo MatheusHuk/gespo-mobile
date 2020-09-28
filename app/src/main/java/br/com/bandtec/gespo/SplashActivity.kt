1ed66e28c4543d5010f874e067d1bd0474595649
MatheusHuk <matheushukmoreschi2000@gmail.com>
2020-09-28 20:11:45 -0300

    Style

--- app/src/main/java/br/com/bandtec/gespo/SplashActivity.kt
+++ app/src/main/java/br/com/bandtec/gespo/SplashActivity.kt
@@ -11,7 +11,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash)
-        check();
+        //check();
     }
 
     private fun check(){
