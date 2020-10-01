045896f82c27ae3a08644e7ab083b6734dd60e30
Bruno1903 <brunoalmeida1960@gmail.com>
2020-10-01 08:58:49 -0300

    Update da nova splash-screen

--- app/src/main/java/br/com/bandtec/gespo/SplashActivity.kt
+++ app/src/main/java/br/com/bandtec/gespo/SplashActivity.kt
@@ -1,6 +1,8 @@
 package br.com.bandtec.gespo
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -8,8 +10,9 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         //check();
+
     }
 
     private fun check(){
