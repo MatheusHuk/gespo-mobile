25b12082ebae67b23b98f7964e7f5725c82780f7
Bruno1903 <brunoalmeida1960@gmail.com>
2020-10-17 16:46:55 -0300

    adiciona tela do timer

---
+++ app/src/main/java/br/com/bandtec/gespo/TimerActivity.kt
@@ -0,0 +1,11 @@
+package br.com.bandtec.gespo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+    }
+}
