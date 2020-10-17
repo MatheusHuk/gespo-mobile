758d77ad649cc3162a9bf3aa0ffa8585f8fba505
MatheusHuk <matheushukmoreschi2000@gmail.com>
2020-10-17 14:20:19 -0300

    Dash

--- app/src/main/java/br/com/bandtec/gespo/MainActivity.kt
+++ app/src/main/java/br/com/bandtec/gespo/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
+import android.text.Layout
+import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
@@ -16,20 +18,24 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import br.com.bandtec.gespo.utils.changeActivity
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.utils.MPPointF
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.LinearLayout.LayoutParams
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.marginBottom
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.*
+import java.text.AttributedCharacterIterator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val c:ColorTemplate = ColorTemplate()
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.selectedItemId = R.id.navigation_dashboard
@@ -48,7 +54,6 @@ class MainActivity : AppCompatActivity() {
         //chartArray.add(PieChart(this))
 
         val chartView: PieChart = PieChart(this)
-        val chartView2: PieChart = PieChart(this)
         //appContainer.addView(newView)
 
         entries.add(PieEntry(50f, "Brazil"))
@@ -87,10 +92,22 @@ class MainActivity : AppCompatActivity() {
             (this.applicationContext.getResources().getDisplayMetrics().density * 400).toInt()
         )
 
-        var set: ConstraintSet = ConstraintSet()
+        val l = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.WRAP_CONTENT
+        )
+        l.bottomMargin = 100
 
-        set.connect(chartView2.id, ConstraintSet.TOP, chartView.id, ConstraintSet.BOTTOM)
-       //set.applyTo(chartView2)
+        var textView = TextView(this)
+        textView.text = "Gráfico Foda 1"
+        textView.layoutParams = l
+        textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+        appContainer.addView(chartView)
+        appContainer.addView(textView)
+
+
+        val chartView2: PieChart = PieChart(this)
 
         chartView2.holeRadius = 0f
         chartView2.transparentCircleRadius = 0f
@@ -105,7 +122,51 @@ class MainActivity : AppCompatActivity() {
             (this.applicationContext.getResources().getDisplayMetrics().density * 400).toInt()
         )
 
-        appContainer.addView(chartView)
+        val textView2 = TextView(this)
+        textView2.text = "Gráfico Foda 2"
+        textView2.layoutParams = l
+        textView2.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+
         appContainer.addView(chartView2)
+        appContainer.addView(textView2)
+
+        val chartView3: BarChart = BarChart(this)
+
+        val barEntries = ArrayList<BarEntry>(0)
+
+        barEntries.add(BarEntry(1f, 10f))
+        barEntries.add(BarEntry(2f, 15f))
+        barEntries.add(BarEntry(3f, 20f))
+        barEntries.add(BarEntry(4f, 17f))
+        barEntries.add(BarEntry(5f, 19f))
+        barEntries.add(BarEntry(6f, 21f))
+
+        val barSet: BarDataSet = BarDataSet(barEntries, "")
+
+        barSet.isVisible = true
+        barSet.setDrawIcons(true)
+        barSet.iconsOffset = MPPointF(0F, 40F)
+        barSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+        barSet.valueTextColor = Color.TRANSPARENT
+
+        chartView3.legend.isEnabled = false
+        chartView3.description.isEnabled = false
+        chartView3.description.textAlign = Paint.Align.CENTER
+        chartView3.setData(BarData(barSet))
+        chartView3.invalidate()
+
+        chartView3.layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            (this.applicationContext.getResources().getDisplayMetrics().density * 400).toInt()
+        )
+
+        val textView3 = TextView(this)
+        textView3.text = "Gráfico Foda, agora em barras"
+        textView3.layoutParams = l
+        textView3.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+        appContainer.addView(chartView3)
+        appContainer.addView(textView3)
     }
 }
