0ec3dfaa6aa0dfe68cb9df7e2af8ca3184a50e47
Your Name <you@example.com>
2020-10-16 21:49:20 -0300

    tela de consulta de apontamentos

--- app/src/main/java/br/com/bandtec/gespo/ui/activity_detailofnote.kt
+++ app/src/main/java/br/com/bandtec/gespo/ui/activity_detailofnote.kt
@@ -1,11 +1,14 @@
 package br.com.bandtec.gespo
 
+import br.com.bandtec.gespo.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity2 : AppCompatActivity() {
+class activity_detailofnote : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_detailofnote)
+
     }
 }
