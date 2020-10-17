6b191d9a68028ee4297ff5e10b619800e6306b53
marcorover <marcomorover@gmail.com>
2020-10-17 14:44:06 -0300

    Filtro de consulta de apontamentos

--- app/src/main/java/br/com/bandtec/gespo/TimesheetConsultActivity.kt
+++ app/src/main/java/br/com/bandtec/gespo/TimesheetConsultActivity.kt
@@ -2,13 +2,21 @@ package br.com.bandtec.gespo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import br.com.bandtec.gespo.utils.changeActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_timesheet_consult.*
 
-class activity_timesheet_consult : AppCompatActivity() {
+class TimesheetConsultActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timesheet_consult)
+
+        //sp_periodo.adapter = ArrayAdapter(this,
+        //R.layout.support_simple_spinner_dropdown_item,
+        //resources.getStringArray(R.array.periodos))
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.selectedItemId = R.id.navigation_apontamentos
