6d993446c59a7750537e954f5732b47480d5f587
Your Name <you@example.com>
2020-10-23 20:48:40 -0300

    Alteracoes tela detalhes apontamentos

--- app/src/main/java/br/com/bandtec/gespo/Activity_detailofnote.kt
+++ app/src/main/java/br/com/bandtec/gespo/Activity_detailofnote.kt
@@ -1,14 +1,12 @@
 package br.com.bandtec.gespo
 
-import br.com.bandtec.gespo.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.bandtec.gespo.utils.changeActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class activity_detailofnote : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detailofnote)
-
-    }
+        }
 }
