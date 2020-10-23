c41b729c87d1ef814729d44a2a305d95a8e40da5
marcorover <marcomorover@gmail.com>
2020-10-23 20:41:16 -0300

    Spinner Counter e Timer

--- app/src/main/java/br/com/bandtec/gespo/TimerActivity.kt
+++ app/src/main/java/br/com/bandtec/gespo/TimerActivity.kt
@@ -1,9 +1,11 @@
 package br.com.bandtec.gespo
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import br.com.bandtec.gespo.utils.changeActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_timer.*
 
 class TimerActivity : AppCompatActivity(){
 
@@ -14,6 +16,12 @@ class TimerActivity : AppCompatActivity(){
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.selectedItemId = R.id.navigation_timer
+
+        //esse é o código do spinner
+        //ele ta puxando lá do arquivo strings.xml as opções
+        sp_options.adapter = ArrayAdapter(this,
+        R.layout.support_simple_spinner_dropdown_item,
+        resources.getStringArray(R.array.options))
 
         navView.setOnNavigationItemSelectedListener(
             BottomNavigationView.OnNavigationItemSelectedListener { item ->
